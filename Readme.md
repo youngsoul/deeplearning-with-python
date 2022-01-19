43a4761d44a8cc0f4fa52fa22a6b7be6a6d88dd2
patrickryan <theyoungsoul@gmail.com>
2022-01-18 19:04:29 -0600

    initial

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# DeepLearning with Python
+
+Repo contains my work going through the book, 
+
+'Deep Learning with Python 2nd Edition'
+
+by
+
+Francois Chollet
+
+
+## Setup
+
+### Local Computer
+
+```shell
+pip install -r requirements.txt
+```
+
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='deeplearning-with-python',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='patrick ryan',
+    author_email='theyoungsoul@gmail.com',
+    description='DeepLearning with Python',
+    install_requires = [
+            'sklearn',
+            'tensorflow',
+            'numpy',
+            'pandas',
+            'matplotlib',
+            'jupyter'
+        ]
+
+)
